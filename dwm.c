165c0c8a2a13a65c95fcaa1e8691d13338ac37f7
even311379 <even311379@hotmail.com>
2023-11-08 13:42:37 +0800

    remove wrap patch

--- dwm.c
+++ dwm.c
@@ -312,7 +312,6 @@ static void updateicon(Client *c);
 static void updatewindowtype(Client *c);
 static void updatewmhints(Client *c);
 static void view(const Arg *arg);
-static void warp(const Client *c);
 static Client *wintoclient(Window w);
 static Monitor *wintomon(Window w);
 static int wmclasscontains(Window win, const char *class, const char *name);
@@ -1171,7 +1170,6 @@ void focusmon(const Arg *arg) {
   unfocus(selmon->sel, 0);
   selmon = m;
   focus(NULL);
-  warp(selmon->sel);
 }
 
 void focusstack(const Arg *arg) {
@@ -1954,9 +1952,6 @@ void restack(Monitor *m) {
         wc.sibling = c->win;
       }
   }
-  if (m == selmon && (m->tagset[m->seltags] & m->sel->tags) &&
-      m->lt[m->sellt]->arrange != &monocle)
-    warp(m->sel);
   XSync(dpy, False);
   while (XCheckMaskEvent(dpy, EnterWindowMask, &ev))
     ;
@@ -2879,24 +2874,6 @@ void view(const Arg *arg) {
   focus(selmon->pertag->sel[selmon->pertag->curtag]);
 
   arrange(selmon);
-}
-
-void warp(const Client *c) {
-  int x, y;
-
-  if (!c) {
-    XWarpPointer(dpy, None, root, 0, 0, 0, 0, selmon->wx + selmon->ww / 2,
-                 selmon->wy + selmon->wh / 2);
-    return;
-  }
-
-  if (!getrootptr(&x, &y) ||
-      (x > c->x - c->bw && y > c->y - c->bw && x < c->x + c->w + c->bw * 2 &&
-       y < c->y + c->h + c->bw * 2) ||
-      (y > c->mon->by && y < c->mon->by + bh) || (c->mon->topbar && !y))
-    return;
-
-  XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w / 2, c->h / 2);
 }
 
 pid_t winpid(Window w) {
--- layoutmenu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cat <<EOF | xmenu
-[]= Tiled Layout			0
-TTT BStack Layout			1
-[@] Spiral Layout			2
-[\] Dwindle Layout			3
